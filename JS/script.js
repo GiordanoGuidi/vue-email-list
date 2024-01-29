13b580d1340d4f677b8277848be567c675f994a1
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-29 19:13:11 +0100

    added cicle for in created

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,23 @@
-const {createApp} = Vue
+const {createApp} = Vue;
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const app = createApp({
+    data:()=>({
+        emailList:[],
+    }),
+    methods:{
+
+    },
+    created(){
+        for(let i = 0; i < 10; i++){
+            axios.get(endpoint)
+            .then((res)=>{
+                this.emailList.push(res.data.response)
+            })
+        }
+        console.log(this.emailList)
+    }
+    
+    
     
 }) 
 app.mount('#root')
